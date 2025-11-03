57603460bb0341d48f5282bc9692e25ed79be2ef
Arul Dsena <103011363+whisper556@users.noreply.github.com>
2025-11-02 19:45:53 -0600

    Fix formatting and add newline at end of shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -11,12 +11,12 @@
 #include "Tokenizer.h"
 #include <fcntl.h>
 
-#define RED     "\033[1;31m"
-#define GREEN   "\033[1;32m"
-#define YELLOW  "\033[1;33m"
-#define BLUE    "\033[1;34m"
-#define WHITE   "\033[1;37m"
-#define NC      "\033[0m"
+#define RED      "\033[1;31m"
+#define GREEN    "\033[1;32m"
+#define YELLOW   "\033[1;33m"
+#define BLUE     "\033[1;34m"
+#define WHITE    "\033[1;37m"
+#define NC       "\033[0m"
 
 using namespace std;
 
